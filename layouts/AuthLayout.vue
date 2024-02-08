0ca5d7a41fba0d20fac97c416c0e9d8cecaab1bc
jurecapuder <capuder.j@gmail.com>
2024-02-08 22:00:59 +0100

    Refactor AuthLayout.vue: Update layout structure and add NuxtLink

--- layouts/AuthLayout.vue
+++ layouts/AuthLayout.vue
@@ -1,6 +1,16 @@
 <template>
-  <div>
-    
+  <div
+    id="AuthLayout"
+    class="h-screen w-full flex justify-between"
+  >
+    <div class="lg:pt-7 pt-3 lg:px-12 px-6 lg:w-2/3 w-full lg:min-w-[800px]">
+      <NuxtLink
+        to="/"
+        class="inline-block"
+      >
+        
+      </NuxtLink>
+    </div>
   </div>
 </template>
 
