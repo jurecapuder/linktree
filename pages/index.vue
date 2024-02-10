56119b144cef69b5a66d2d8d5d3b18f973143fe2
jurecapuder <capuder.j@gmail.com>
2024-02-10 23:46:13 +0100

    Add login page title

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,13 @@
 <template>
   <AuthLayout>
-
+    <div class="mt-10">
+      <h1 class="lg:text-5xl text-3xl text-center font-extrabold">
+        Log in to your Linktree
+      </h1>
+    </div>
   </AuthLayout>
 </template>
 
 <script setup>
-  import AuthLayout from '~/layouts/AuthLayout.vue';
+  import AuthLayout from '~~/layouts/AuthLayout.vue';
 </script>
