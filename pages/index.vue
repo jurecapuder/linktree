7b97055e2fe527ccf2c10290bac0fd579798ec53
jurecapuder <capuder.j@gmail.com>
2024-02-10 23:50:21 +0100

    Add login form to index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,20 @@
       <h1 class="lg:text-5xl text-3xl text-center font-extrabold">
         Log in to your Linktree
       </h1>
+
+      <form
+        class="mt-12"
+        @submit.prevent="$event => login()"
+      >
+      <div>
+        <TextInput
+          placeholder="Email: link@gmail.com"
+          v-model:input="email"
+          inputType="email"
+          :error="errors && errors.email ? errors.email[0] : ''"
+        />
+      </div>
+    </form>
     </div>
   </AuthLayout>
 </template>
