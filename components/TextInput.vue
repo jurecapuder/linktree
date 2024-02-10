be5e1b9a2d0d685e1489e804b2d630692ca937a1
jurecapuder <capuder.j@gmail.com>
2024-02-10 23:56:00 +0100

    Update TextInput component styling and behavior

--- components/TextInput.vue
+++ components/TextInput.vue
@@ -7,7 +7,11 @@
       class="w-full bg-[#EFF0EB] text-gray-800 border-2 text-sm border-[#EFF0EB] rounded-xl py-3.5 px-3 placeholder-gray-500 focus:outline-none"
       @focus="$event => isFocused = true"
       @blur="$event => isFocused = false"
-    />
+      :class="isFocused ? 'border-gray-900' : ''"
+      :type="inputType"
+      v-model="inputComputed"
+      autocomplete="off"
+    >
   </div>
 </template>
 
