684faeaa077f12c7d942f305bca26ab76a53dcf6
jurecapuder <capuder.j@gmail.com>
2024-02-11 23:34:50 +0100

    Add submit button to login form

--- pages/index.vue
+++ pages/index.vue
@@ -27,6 +27,17 @@
           :error="errors && errors.password ? errors.password[0] : ''"
         />
       </div>
+
+      <div class="mt-10">
+        <button
+          type="submit"
+          class="rounded-full w-full p-3 font-bold"
+          :disabled="(!email || !password)"
+          :class="(email && password) ? 'bg-[#8228D9] hover:bg-[#6C21B3] text-white' : 'bg-[#EFF0EB] text-[#A7AAA2]'"
+        >
+
+        </button>
+      </div>
     </form>
     </div>
   </AuthLayout>
