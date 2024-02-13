2542424e050b259ca3d3928aa75fa9c42109f31f
jurecapuder <capuder.j@gmail.com>
2024-02-13 23:03:01 +0100

    Add TopNav component to AdminLayout.vue

--- layouts/AdminLayout.vue
+++ layouts/AdminLayout.vue
@@ -3,10 +3,18 @@
     id="AdminLayout"
     class="w-full fixed z-30"
   >
-    
+    <div
+      v-if="!useUserStore.isMobile"
+      id="TopNav"
+      class="w-full flex items-center justify-between md:pt-2.5 md:px-2.5"
+    >
+
+    </div>
   </div>
 </template>
 
 <script setup>
-  
+  import { useUserStore } from "~~/stores/user";
+
+  const userStore = useUserStore();
 </script>
