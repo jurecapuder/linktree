035bf169f73834b679506f15ccc68a5888f3aca6
jurecapuder <capuder.j@gmail.com>
2024-02-13 23:06:09 +0100

    Add imports and variables for user store in app.vue

--- app.vue
+++ app.vue
@@ -5,6 +5,12 @@
 </template>
 
 <script setup>
+  import { useUserStore } from "~~/stores/user";
+  import { storeToRefs } from "pinia";
+
+  const userStore = useUserStore();
+  const { isMobile, isPreviewOverlay, updatedLinkId, addLinkOverlay, id } = storeToRefs(userStore);
+
   onMounted(() => {
     if ("ontouchstart" in window) {
       isMobile.value = true;
