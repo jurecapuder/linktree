f6cb63a041b431d8397527a78428e82c9d337133
jurecapuder <capuder.j@gmail.com>
2024-02-14 22:56:31 +0100

    Add linktree logo to AdminLayout.vue

--- layouts/AdminLayout.vue
+++ layouts/AdminLayout.vue
@@ -9,7 +9,14 @@
       class="w-full flex items-center justify-between md:pt-2.5 md:px-2.5"
     >
       <div class="flex items-center justify-between md:rounded-full md:shadow-sm px-2 md:pl-6 pl-4 py-[8px] bg-white w-full border-b">
-        <div class="flex items-center justify-start w-full max-w-[500px]"></div>
+        <div class="flex items-center justify-start w-full max-w-[500px]">
+          <LuxtLink to="/admin">
+            <img
+              class="w-[23px] min-w-[23px] select-none"
+              src="~/assets/images/linktree-logo-icon.png"
+            />
+          </LuxtLink>
+        </div>
       </div>
     </div>
   </div>
