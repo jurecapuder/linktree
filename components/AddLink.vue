983091426662464c7190e7f46ff9e6d70d9cc80f
jurecapuder <capuder.j@gmail.com>
2024-02-26 22:37:09 +0100

    Refactor AddLink component to include URL input field

--- components/AddLink.vue
+++ components/AddLink.vue
@@ -1,9 +1,16 @@
 <template>
-  <div>
-    
+  <div
+    id="AddLink"
+    class="w-full bg-white rounded-3xl overflow-hidden"
+  >
+    <div class="flex items-center justify-between pb-2 p-6">
+      <div class="text-[19px] font-semibold">
+        Enter URL
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-  
+
 </script>
