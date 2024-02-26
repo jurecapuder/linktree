93db2c5a069c046dfe0832dcb2103efcf502b083
jurecapuder <capuder.j@gmail.com>
2024-02-26 22:38:49 +0100

    Add close button to AddLink component

--- components/AddLink.vue
+++ components/AddLink.vue
@@ -7,6 +7,17 @@
       <div class="text-[19px] font-semibold">
         Enter URL
       </div>
+
+      <button
+        @click="$event => $emit('close')"
+        class="flex items-center rounded-full p-1.5 hover:bg-[#EFF0EA]"
+      >
+        <Icon
+          name="mdi:close"
+          size="20"
+          color="#676B5F"
+        />
+      </button>
     </div>
   </div>
 </template>
