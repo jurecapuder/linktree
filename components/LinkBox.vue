bc18366fbaebf4cb3982448b20d8623b23e3ddf7
jurecapuder <capuder.j@gmail.com>
2024-02-29 23:12:19 +0100

    Refactor LinkBox component

--- components/LinkBox.vue
+++ components/LinkBox.vue
@@ -1,6 +1,18 @@
 <template>
-  <div>
-    
+  <div
+    id="LinkBox"
+    class="w-full bg-white rounded-3xl"
+  >
+    <div
+      id="MainLinkBoxSection"
+      class="px-8 py-5"
+    >
+      <div class="flex items-center justify-between py-1">
+        <div class="flex items-center w-full">
+          
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
