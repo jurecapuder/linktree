db1cf2513768b27000b2601005f37b956655513b
jurecapuder <capuder.j@gmail.com>
2024-02-29 23:13:29 +0100

    Add input field for editing name in LinkBox.vue

--- components/LinkBox.vue
+++ components/LinkBox.vue
@@ -9,7 +9,14 @@
     >
       <div class="flex items-center justify-between py-1">
         <div class="flex items-center w-full">
-          
+          <input
+            v-if="true"
+            id="editNameInput"
+            type="text"
+            v-model="name"
+            maxlength="18"
+            class="w-full text-sm font-semibold focus:outline-none"
+          />
         </div>
       </div>
     </div>
