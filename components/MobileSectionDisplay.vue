948f0c93666f3697f2942c01b456361c96a5fc86
jurecapuder <capuder.j@gmail.com>
2024-03-14 23:03:29 +0100

    Refactor MobileSectionDisplay.vue: Update mobile section display layout

--- components/MobileSectionDisplay.vue
+++ components/MobileSectionDisplay.vue
@@ -1,6 +1,11 @@
 <template>
-    <div>
-
+    <div class="md:block fixed hidden right-0 lg:w-[500px] w-[310px] h-[calc(100%-20px)] mt-[20px] mx-auto border-l border-l-gray-300 pt-20">
+        <div class="mx-auto mt-16 mb-16 flex justify-center items-center w-full lg:max-w-[230px] max-w-[200px] lg:h-[460px] h-[400px] p-3 rounded-3xl relative">
+            <img
+                class="absolute z-10 pointer-events-none select-none"
+                src="~/assets/images/mobile-case.png"
+            />
+        </div>
     </div>
 </template>
 
