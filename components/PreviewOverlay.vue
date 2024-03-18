f4be6357f6cd9727d5774bd00ad9f87aac5de9b6
jurecapuder <capuder.j@gmail.com>
2024-03-18 23:23:04 +0100

    Refactor PreviewOverlay component

--- components/PreviewOverlay.vue
+++ components/PreviewOverlay.vue
@@ -1,9 +1,15 @@
 <template>
-    <div>
+    <div
+        id="PreviewOverlay"
+        class="md:hidden fixed z-30 top-0 left-0 h-full w-full overflow-auto"
+        :class="userStore.theme"
+    >
 
     </div>
 </template>
 
 <script setup>
+    import { useUserStore } from "~~/stores/user";
 
+    const userStore = useUserStore();
 </script>
