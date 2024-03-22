0044bb4ad786fff10e2d1ae387c79333f64b83e6
jurecapuder <capuder.j@gmail.com>
2024-03-22 22:43:55 +0100

    Refactor apperance.vue component and import useUserStore from user store

--- pages/admin/apperance.vue
+++ pages/admin/apperance.vue
@@ -3,12 +3,18 @@
         id="ApperancePage"
         class="flex h-[calc(100%-50px)] pb-4"
     >
-        <div>
-            
+        <div class="lg:w-[calc(100%-500px)] md:w-[calc(100%-330px)] w-full md:pt-20 pt-14">
+            <div class="max-w-[750px] mx-auto pb-24">
+                
+            </div>
         </div>
     </AdminLayout>
 </template>
 
 <script setup>
     import AdminLayout from "~~/layouts/AdminLayout.vue";
+
+    import { useUserStore } from "~~/stores/user";
+
+    const userStore = useUserStore();
 </script>
