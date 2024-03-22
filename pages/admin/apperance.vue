323b824d53b4797062b8a1a88d2646fb108416f8
jurecapuder <capuder.j@gmail.com>
2024-03-22 22:40:05 +0100

    Add apperance page to admin section

---
+++ pages/admin/apperance.vue
@@ -0,0 +1,14 @@
+<template>
+    <AdminLayout
+        id="ApperancePage"
+        class="flex h-[calc(100%-50px)] pb-4"
+    >
+        <div>
+            
+        </div>
+    </AdminLayout>
+</template>
+
+<script setup>
+    import AdminLayout from "~~/layouts/AdminLayout.vue";
+</script>
