ffc3a6ffdc44370ffb26fa6f4249bcb33c15ef27
jurecapuder <capuder.j@gmail.com>
2024-03-28 23:52:48 +0100

    Add admin preview page

---
+++ pages/admin/preview.vue
@@ -0,0 +1,14 @@
+<template>
+    <div
+        class="fixed overflow-auto w-full h-screen"
+        :class="userStore.theme"
+    />
+
+    <div>
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
